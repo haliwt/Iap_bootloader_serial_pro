1486c5726c92badb8d52d69bfabad8653fc7ac12
haliwt <jonewang@126.com>
2023-09-05 13:58:59 +0800

    Add ymodem of protocol .

---
+++ Src/iap.c
@@ -0,0 +1,115 @@
+/**
+ ****************************************************************************************************
+ * @file        iap.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2020-05-12
+ * @brief       IAP 代码
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 STM32F103开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ * 修改说明
+ * V1.0 20200512
+ * 第一次发布
+ *
+ ****************************************************************************************************
+ */
+
+#include "iap.h"
+#include "stmflash.h"
+
+
+iapfun jump2app;
+
+typedef  void (*pFunction)(void);
+#define APPLICATION_ADDRESS     (uint32_t)0x08004000      /* Start user code address: ADDR_FLASH_PAGE_8 */
+pFunction JumpToApplication;
+uint32_t JumpAddress_local;
+
+uint16_t g_iapbuf[1024];       /* 2K字节缓存 */
+static void sys_msr_msp(uint32_t addr);
+/**
+ * @brief       IAP写入APP BIN
+ * @param       appxaddr : 应用程序的起始地址
+ * @param       appbuf   : 应用程序CODE
+ * @param       appsize  : 应用程序大小(字节)
+ * @retval      无
+ */
+void iap_write_appbin(uint32_t appxaddr, uint8_t *appbuf, uint32_t appsize)
+{
+    uint16_t t;
+    uint16_t i = 0;
+    uint16_t temp;
+    uint32_t fwaddr = appxaddr; /* 当前写入的地址 */
+    uint8_t *dfu = appbuf;
+
+    for (t = 0; t < appsize; t += 2)
+    {
+        temp = (uint16_t)dfu[1] << 8;
+        temp |= (uint16_t)dfu[0];
+        dfu += 2;               /* 偏移2个字节 */
+        g_iapbuf[i++] = temp;
+
+        if (i == 1024)
+        {
+            i = 0;
+            stmflash_write(fwaddr, g_iapbuf, 1024);
+         
+            fwaddr += 2048;     /* 偏移2048  16 = 2 * 8  所以要乘以2 */
+        }
+    }
+
+    if (i)
+    {
+        stmflash_write(fwaddr, g_iapbuf, i);  /* 将最后的一些内容字节写进去 */
+      
+    }
+}
+
+/**
+ * @brief       跳转到应用程序段(执行APP)
+ * @param       appxaddr : 应用程序的起始地址
+
+ * @retval      无
+ */
+void iap_load_app(uint32_t appxaddr)
+{
+//  //  if (((*(volatile  uint32_t *)appxaddr) & 0x2FFE0000) == 0x20000000)     /* 检查栈顶地址是否合法.可以放在内部SRAM共64KB(0x20000000) */
+//    {
+//        /* 用户代码区第二个字为程序开始地址(复位地址) */
+//        jump2app = (iapfun) * (volatile uint32_t *)(appxaddr + 4);
+//        
+//        /* 初始化APP堆栈指针(用户代码区的第一个字用于存放栈顶地址) */
+//        sys_msr_msp(*(volatile uint32_t *)appxaddr);
+//        
+//        /* 跳转到APP */
+//        jump2app();
+//    }
+     JumpAddress_local= *(__IO uint32_t*) (APPLICATION_ADDRESS + 4);
+      JumpToApplication = (pFunction) JumpAddress_local;
+      /* Initialize user application's Stack Pointer */
+      __set_MSP(*(__IO uint32_t*) APPLICATION_ADDRESS);
+      JumpToApplication();
+}
+
+static void sys_msr_msp(uint32_t addr)
+{
+    __set_MSP(addr);    /* 设置栈顶地址 */
+}
+
+
+
+
+
+
+
+
+
+
